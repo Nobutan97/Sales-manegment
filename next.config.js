56fba8a73b06b3a7332dcf47aea780d98becce1a
Nobutan97 <your.email@example.com>
2025-03-25 22:04:03 +0900

    feat: GitHub Pages用の設定を追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Sales-manegment',
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
